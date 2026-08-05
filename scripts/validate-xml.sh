aef131b88714763944dea481871b6724bc84a5f3
Cody Black <coffee8lack@gmail.com>
2026-08-04 21:25:28 -0500

    Use xargs to keep return code from xmllint

--- scripts/validate-xml.sh
+++ scripts/validate-xml.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-find . -type f -name *.xml -size +0 -exec xmllint --noout {} \;
+
+find . -type f -name *.xml -size +0 -print0 | xargs -0 -i xmllint --noout {}
